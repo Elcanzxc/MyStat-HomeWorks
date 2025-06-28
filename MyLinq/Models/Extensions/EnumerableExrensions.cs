7f51221f736f6fa64f1a6436edd676b56fa189bf
Domashnie Zadaniya <Elcan4361@gmail.com>
2025-06-28 23:15:41 +0400

    Linq

---
+++ MyLinq/Models/Extensions/EnumerableExrensions.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinq.Models.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool All<T>(this IEnumerable<T> collection , Func<T,bool> predicate)
+        {
+            foreach(T element in collection)
+            {
+                if (!predicate(element))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool Any<T>(this IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                return true;
+            }
+               
+            return false;
+        }
+
+        public static bool Any<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                    return true;
+            }
+            return false;
+        }
+
+        public static T First<T>(this IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                return item;
+            }
+
+            throw new InvalidOperationException("Не содержит элементов");
+
+        }
+
+        public static T First<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+                    
+            }
+
+            throw new InvalidOperationException("Не содержит элементов удовлетворяющих условие");
+        }
+
+        public static T FirstOrDefault<T>(this IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                return item;
+            }
+               
+            return default;
+        }
+
+        public static T FirstOrDefault<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+                   
+            }
+
+            return default;
+        }
+
+        public static T FirstOrDefault<T>(this IEnumerable<T> collection, Func<T, bool> predicate,T defaultValue)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+            }
+
+            return defaultValue;
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            var result = new List<T>();
+
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+
+            return result;
+        }
+
+        public static int Count<T>(this IEnumerable<T> collection)
+        {
+            int count = 0;
+            foreach (var item in collection)
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public static int Count<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            int count = 0;
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ MyLinq/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using MyLinq.Models.Extensions;
+using System;
+using System.Collections.Generic;
+
+// У меня имя метода конфликтовал с методами из Linq , так что я отключил   <ImplicitUsings>disable</ImplicitUsings>
+class Program
+{
+    public static string ShowNames<T>(IEnumerable<T> obj)
+    {
+        string names = "";
+        foreach (var name in obj)
+        {
+            names+= name + " ";
+        }
+        return names;
+    }  // Создал метод который выводит имена ( сделал чисто для того чтобы не портить эстетику в Main части)
+    static void Main()
+    {
+        string[] names = { "Elcan", "Kamran", "Ilham", "Rashad" };
+
+
+
+        Console.WriteLine($"Names: {ShowNames(names)}\n");
+       
+        Console.WriteLine($"Is all names contains 'a' : {names.All(name => name.Contains('a'))}\n");
+
+        Console.WriteLine($"Is names have elements: {names.Any()}\n");
+
+        Console.WriteLine($"Is any names contains 'h' : {names.Any(name => name.Contains('h'))}\n");
+
+        Console.WriteLine($"First element of names: {names.First()}\n");
+
+        Console.WriteLine($"First element contain 'm': {names.First(name => name.Contains('m'))}\n");
+
+        Console.WriteLine($"First or Default element of names: {names.FirstOrDefault()}\n");
+
+        Console.WriteLine($"First or Default element contain 'z': {names.FirstOrDefault(name => name.Contains('z'))}\n");
+
+        string? result = names.FirstOrDefault(name => name.Contains('z'),"NONE");
+        Console.WriteLine($"First or Default(My Default) element contain 'z': {result}\n");
+
+        Console.WriteLine($"Names where length <= 5:{ShowNames<string>(names.Where(name => name.Length <= 5))} \n");
+
+        Console.WriteLine($"Names count : {names.Count()}\n");
+
+        Console.WriteLine($"Names count which contains 'm':  {names.Count(name => name.Contains('m'))}\n");
+    }
+}

3b27fefe8adeab6b79c5e754bc847e4c98f30284
Domashnie Zadaniya <Elcan4361@gmail.com>
2025-09-26 01:32:32 +0400

    Tasks-Parallel

---
+++ C# HomeWorks/Tasks-Parallel/Models/EntityAsyncSqlRepository.cs
@@ -0,0 +1,85 @@
+﻿using Async_Await.Entities;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace UserProfileApp.Repositories
+{
+    public class EntityAsyncSqlRepository
+    {
+        private static readonly string connectionString =
+            "Server=localhost;Database=UserProfile;Integrated Security=True;TrustServerCertificate=True;";
+
+        public EntityAsyncSqlRepository() { }
+
+        public Task Create(User user)
+        {
+            return Task.Run(() =>
+            {
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                string sql = "INSERT INTO Users (Name, Surname, BirthDate) VALUES (@Name, @Surname, @BirthDate)";
+                connection.Execute(sql, user);
+            });
+        }
+
+        public Task<User?> GetById(int id)
+        {
+            return Task.Run(() =>
+            {
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                string sql = "SELECT * FROM Users WHERE Id = @Id";
+                return connection.QueryFirstOrDefault<User>(sql, new { Id = id });
+            });
+        }
+
+        public Task<IEnumerable<User>> GetAll()
+        {
+            return Task.Run(() =>
+            {
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                string sql = "SELECT * FROM Users";
+                return connection.Query<User>(sql);
+            });
+        }
+
+        public Task Update(User user)
+        {
+            return Task.Run(() =>
+            {
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                string sql = "UPDATE Users SET Name = @Name, Surname = @Surname, BirthDate = @BirthDate WHERE Id = @Id";
+                connection.Execute(sql, user);
+            });
+        }
+
+        public Task Delete(int id)
+        {
+            return Task.Run(() =>
+            {
+                using var connection = new SqlConnection(connectionString);
+                connection.Open();
+                string sql = "DELETE FROM Users WHERE Id = @Id";
+                connection.Execute(sql, new { Id = id });
+            });
+        }
+
+        public Task BulkInsertParallel(IEnumerable<User> users)
+        {
+            return Task.Run(() =>
+            {
+                Parallel.ForEach(users, user =>
+                {
+                    using var connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    string sql = "INSERT INTO Users (Name, Surname, BirthDate) VALUES (@Name, @Surname, @BirthDate)";
+                    connection.Execute(sql, user);
+                });
+            });
+        }
+    }
+}
---
+++ C# HomeWorks/Tasks-Parallel/Program.cs
@@ -0,0 +1,56 @@
+﻿using Async_Await.Entities;
+using Tasks_Parallel.Models;
+using UserProfileApp.Repositories;
+
+class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+          var users1 = new List<User>
+            {
+                new User { Name = "Kemale", Surname = "Samirova", BirthDate = new DateTime(2006, 5, 5) },
+                new User { Name = "Leyla", Surname = "Mammadova", BirthDate = new DateTime(2005, 8, 12) }
+            };
+
+
+        var entitySyncSqlRepository = new EntitySyncSqlRepository();
+
+
+            entitySyncSqlRepository.BulkInsert(users1);
+               
+            Console.ReadLine();
+         
+            var allSyncUsers = entitySyncSqlRepository.GetAll();
+            foreach (var u in allSyncUsers)
+            {
+                Console.WriteLine($"User -> {u.Id}: {u.Name} {u.Surname} {u.BirthDate:yyyy-MM-dd}");
+            }
+
+
+           Console.ReadLine();
+
+
+
+        var users2 = new List<User>
+            {
+                new User { Name = "Anton", Surname = "xzz", BirthDate = new DateTime(2006, 5, 5) },
+                new User { Name = "Sanya", Surname = "tojexz", BirthDate = new DateTime(2005, 8, 12) }
+            };
+        var entityAsyncSqlRepository = new EntityAsyncSqlRepository();
+
+          var usersTasks = entityAsyncSqlRepository.BulkInsertParallel(users2);
+    
+
+            Console.ReadLine();
+            var allAsyncUsers =  entityAsyncSqlRepository.GetAll();
+            var allUsers = allAsyncUsers.Result;
+
+            foreach (var u in allUsers)
+            {
+                Console.WriteLine($"User -> {u.Id}: {u.Name} {u.Surname} {u.BirthDate:yyyy-MM-dd}");
+            }
+
+
+        }
+    }
